6fc236c86831a8c9c86d51dae9d3b7271802193a
Rob Thomure <robthomure@outlook.com>
2023-08-26 15:41:44 -0500

    created separate methods for each action in the switch statement of main. also converted switch statement in main to switch expression.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,56 +3,63 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
+    static Scanner keyboard = new Scanner(System.in);
+    static CoffeeMachine coffeeMachine = new CoffeeMachine();
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        CoffeeMachine coffeeMachine = new CoffeeMachine();
-
         while (true) {
             coffeeMachine.currentSate = CurrentState.CHOOSING_ACTION;
             System.out.println("Write action (buy, fill, take, remaining, exit):");
-            String action = scanner.nextLine();
+            String action = keyboard.nextLine();
             System.out.println();
             switch (action) {
-                case "buy":
-                    coffeeMachine.currentSate = CurrentState.CHOOSING_COFFEE_VARIANT;
-                    System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to " +
-                            "main menu:");
-                    String coffeeType = scanner.nextLine();
-                    if (!coffeeType.equals("back")) {
-                        coffeeMachine.processInput(coffeeType);
-                    }
-                    break;
-                case "fill":
-                    coffeeMachine.currentSate = CurrentState.FILLING_COFFEE_MACHINE;
-                    System.out.println("Write how many ml of water you want to add:");
-                    String input = scanner.nextLine() + " ";
-                    System.out.println("Write how many ml of milk you want to add:");
-                    input += scanner.nextLine() + " ";
-                    System.out.println("Write how many grams of coffee beans you want to add:");
-                    input += scanner.nextLine() + " ";
-                    System.out.println("Write how many disposable cups of coffee you want to add:");
-                    input += scanner.nextLine();
-                    System.out.println();
-                    coffeeMachine.processInput(input);
-                    break;
-                case "take":
-                    coffeeMachine.currentSate = CurrentState.TAKE_MONEY;
-                    coffeeMachine.processInput("");
-                    break;
-                case "remaining":
-                    coffeeMachine.currentSate = CurrentState.CHECK_REMAINING;
-                    coffeeMachine.processInput("");
-                    break;
-                case "exit":
+                case "buy" -> buy(coffeeMachine);
+                case "fill" -> fill(coffeeMachine);
+                case "take" -> take(coffeeMachine);
+                case "remaining" -> remaining(coffeeMachine);
+                case "exit" -> {
                     return;
-                default:
-                    System.out.println("Invalid choice");
-                    break;
+                }
+                default -> System.out.println("Invalid choice");
             }
         }
 
     }
+
+    public static void buy(CoffeeMachine coffeeMachine) {
+        coffeeMachine.currentSate = CurrentState.CHOOSING_COFFEE_VARIANT;
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to " +
+                "main menu:");
+        String coffeeType = keyboard.nextLine();
+        if (!coffeeType.equals("back")) {
+            coffeeMachine.processInput(coffeeType);
+        }
+    }
+
+    public static void fill(CoffeeMachine coffeeMachine) {
+        coffeeMachine.currentSate = CurrentState.FILLING_COFFEE_MACHINE;
+        System.out.println("Write how many ml of water you want to add:");
+        String input = keyboard.nextLine() + " ";
+        System.out.println("Write how many ml of milk you want to add:");
+        input += keyboard.nextLine() + " ";
+        System.out.println("Write how many grams of coffee beans you want to add:");
+        input += keyboard.nextLine() + " ";
+        System.out.println("Write how many disposable cups of coffee you want to add:");
+        input += keyboard.nextLine();
+        System.out.println();
+        coffeeMachine.processInput(input);
+    }
+
+    public static void take(CoffeeMachine coffeeMachine) {
+        coffeeMachine.currentSate = CurrentState.TAKE_MONEY;
+        coffeeMachine.processInput("");
+    }
+
+    public static void remaining(CoffeeMachine coffeeMachine) {
+        coffeeMachine.currentSate = CurrentState.CHECK_REMAINING;
+        coffeeMachine.processInput("");
+    }
+
 
 }
 
